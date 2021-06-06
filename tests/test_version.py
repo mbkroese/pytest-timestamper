160341bf44447e2c009cfb435ab54a2df3042913
mbk <mbk@mbks-MBP.lan>
2021-06-06 11:10:07 +0200

    Updated config of scm_version

--- tests/test_version.py
+++ tests/test_version.py
@@ -2,7 +2,4 @@ from pytest_timestamper import _version
 
 
 def test_has_version():
-    major, minor, patch = _version.version_tuple
-    assert major is not None
-    assert minor is not None
-    assert patch is not None
+    _version.version_tuple is not None
