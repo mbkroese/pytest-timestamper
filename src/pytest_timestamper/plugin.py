1827592bf18762bc0bc17baa74b09bf6f6131865
Tommy Green <tommygreen@Tommys-MacBook-Air.local>
2022-06-22 11:26:37 +0800

    support python2.7, lower version pytest

--- src/pytest_timestamper/plugin.py
+++ src/pytest_timestamper/plugin.py
@@ -2,13 +2,17 @@ import datetime
 import sys
 from typing import Optional, TextIO
 
-from _pytest.config import Config
-from _pytest.config.argparsing import Parser
-from _pytest.terminal import TerminalReporter
 import pytest
+from _pytest.config import Config
+if sys.version_info[0] == 2:
+    from _pytest.config import Parser
+else:
+    from _pytest.config.argparsing import Parser
+from _pytest.terminal import TerminalReporter
 
 
-def pytest_addoption(parser: Parser) -> None:
+def pytest_addoption(parser):
+    # type: (Parser) -> None
     group = parser.getgroup("Adds a timestamp format to pytest output. Currently only works in verbose mode.")
     group.addoption("--datefmt", help="Format that is compatible with `strftime`", default="%Y-%m-%d %H:%M:%S")
     group.addoption(
@@ -19,16 +23,24 @@ def pytest_addoption(parser: Parser) -> None:
 
 
 class TimestamperTerminalReporter(TerminalReporter):
-    def __init__(self, config: Config, file: Optional[TextIO] = None) -> None:
-        super().__init__(config, file)
+    def __init__(self, config, file = None):
+        # type: (Config, Optional[TextIO]) -> None
+        if sys.version_info[0] == 2:
+            TerminalReporter.__init__(self, config, file)
+        else:
+            super().__init__(config, file)
 
         # we need a cache to ensure we only print one line per test
         self._cache = {}
 
-    def _locationline(self, nodeid: str, fspath: str, lineno: Optional[int], domain: str):
+    def _locationline(self, nodeid, fspath, lineno, domain):
+        # type: (str, str, Optional[int], str) -> None
         key = (nodeid, fspath, lineno, domain)
         if key not in self._cache:
-            baseline = super()._locationline(nodeid, fspath, lineno, domain)
+            if sys.version_info[0] == 2:
+                baseline = TerminalReporter._locationline(self, nodeid, fspath, lineno, domain)
+            else:
+                baseline = super()._locationline(nodeid, fspath, lineno, domain)
             formatted_datetime = datetime.datetime.now().strftime(self.config.option.datefmt)
             formatted_prefix = self.config.option.prefixfmt.format(formatted_datetime=formatted_datetime)
             self._cache[key] = formatted_prefix + baseline
@@ -36,7 +48,11 @@ class TimestamperTerminalReporter(TerminalReporter):
 
 
 @pytest.mark.trylast
-def pytest_configure(config: Config) -> None:
+def pytest_configure(config):
+    # type: (Config) -> None
+    if sys.version_info[0] == 2:
+        config.pluginmanager.has_plugin == config.pluginmanager.hasplugin
+        config.pluginmanager.get_plugin == config.pluginmanager.getplugin
     if config.pluginmanager.has_plugin("terminalreporter"):
         reporter = config.pluginmanager.get_plugin("terminalreporter")
         config.pluginmanager.unregister(reporter, "terminalreporter")
